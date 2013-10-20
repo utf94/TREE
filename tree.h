8f304e5bd3656ebf1af726565c9b09e52f30d5e0
Ark2 <abeersaqib948@gmail.com>
2013-10-20 11:12:17 +0500

    initial commit

---
+++ tree.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std ;
+template <typename K >
+class tree
+{
+	class leaf
+	{
+	public:
+		leaf(){}
+		~leaf(){}
+		leaf **node ;
+		K data ;
+	};
+
+
+
+
+};
